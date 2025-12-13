ecd4e58faf48267f86b8272426c0732c664cfb55
Philipp Roshchin <r.phil@yandex.ru>
2025-12-13 17:27:38 +0300

    added ROOMS const

---
+++ Front/react/app/data/rooms.js
@@ -0,0 +1,34 @@
+export var ROOMS = {
+    roomA: {
+        id: "1",
+        name: "Помещение A",
+        points: [{ x: 50, y: 100 }, { x: 240, y: 100 }, { x: 240, y: 260 }, { x: 50, y: 260 }],
+        color: "rgba(248, 249, 250, 0.3)",
+        originalColor: "rgba(248, 249, 250, 0.3)",
+        type: "room"
+    },
+    roomB: {
+        id: "2",
+        name: "Помещение Б",
+        points: [{ x: 250, y: 100 }, { x: 390, y: 100 }, { x: 390, y: 260 }, { x: 250, y: 260 }],
+        color: "rgba(248, 249, 250, 0.3)",
+        originalColor: "rgba(248, 249, 250, 0.3)",
+        type: "room"
+    },
+    roomC: {
+        id: "3",
+        name: "Помещение В",
+        points: [{ x: 500, y: 100 }, { x: 800, y: 100 }, { x: 800, y: 260 }, { x: 500, y: 260 }],
+        color: "rgba(248, 249, 250, 0.3)",
+        originalColor: "rgba(248, 249, 250, 0.3)",
+        type: "room"
+    },
+    roomD: {
+        id: "4",
+        name: "Помещение В1",
+        points: [{ x: 250, y: 280 }, { x: 950, y: 280 }, { x: 950, y: 400 }],
+        color: "rgba(248, 249, 250, 0.3)",
+        originalColor: "rgba(248, 249, 250, 0.3)",
+        type: "room"
+    },
+}
--- Front/react/app/pages/User/User.jsx
+++ Front/react/app/pages/User/User.jsx
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom"
 import Plot from "react-plotly.js"
 import { AuthContext, UserContext } from "../../data/context.js"
 import { HttpMethod, APIVersion } from "../../data/enums.js"
+import { ROOMS } from "../../data/rooms.js"
 import { useSetUser } from "../../hooks/useAuth.js"
 import { notify_success } from "../../modules/notify.js"
 import Fetch from "../../API/Fetch.js"
@@ -73,40 +74,7 @@ export default function App() {
     var [rooms, setRooms] = useState([])
 
     var initializeRooms = () => {
-        return {
-            roomA: {
-                id: "1",
-                name: "Помещение A",
-                points: [{ x: 50, y: 100 }, { x: 240, y: 100 }, { x: 240, y: 260 }, { x: 50, y: 260 }],
-                color: "rgba(248, 249, 250, 0.3)",
-                originalColor: "rgba(248, 249, 250, 0.3)",
-                type: "room"
-            },
-            roomB: {
-                id: "2",
-                name: "Помещение Б",
-                points: [{ x: 250, y: 100 }, { x: 390, y: 100 }, { x: 390, y: 260 }, { x: 250, y: 260 }],
-                color: "rgba(248, 249, 250, 0.3)",
-                originalColor: "rgba(248, 249, 250, 0.3)",
-                type: "room"
-            },
-            roomC: {
-                id: "3",
-                name: "Помещение В",
-                points: [{ x: 500, y: 100 }, { x: 800, y: 100 }, { x: 800, y: 260 }, { x: 500, y: 260 }],
-                color: "rgba(248, 249, 250, 0.3)",
-                originalColor: "rgba(248, 249, 250, 0.3)",
-                type: "room"
-            },
-            roomD: {
-                id: "4",
-                name: "Помещение В1",
-                points: [{ x: 250, y: 280 }, { x: 950, y: 280 }, { x: 950, y: 400 }],
-                color: "rgba(248, 249, 250, 0.3)",
-                originalColor: "rgba(248, 249, 250, 0.3)",
-                type: "room"
-            },
-        }
+        return ROOMS
     }
 
     // Обновление размеров canvas
